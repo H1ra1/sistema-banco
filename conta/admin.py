8ca088ae74fe9131c832f4f88ef62124d8c014e1
Hira <hlira96@gmail.com>
2019-07-31 19:37:07 -0300

    registrar model e configurar forma de exibição no painel

--- conta/admin.py
+++ conta/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from conta import models
 
-admin.site.register(models.Titular)
-admin.site.register(models.Conta)
+class Titular_admin(admin.ModelAdmin):
+    list_display = ['id', 'nome', 'sobrenome', 'email', 'telefone', 'data_criacao']
+
+
+class Conta_admin(admin.ModelAdmin):
+    list_display = ['titular', 'numero_conta', 'agencia', 'nome_banco' ,'saldo', 'ativo']
+    search_fields = ['titular', 'numero_conta']
+
+admin.site.register(models.Titular, Titular_admin)
+admin.site.register(models.Conta, Conta_admin)
