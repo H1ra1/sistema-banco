c6e6f9208a2035b21a5707e45e60d6b287055c13
Hira <hlira96@gmail.com>
2019-08-03 21:51:22 -0300

    alterar visualização das tabelas no django admin

--- conta/admin.py
+++ conta/admin.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
-from conta import models
+from .models import User, Perfil, Conta
 
 class Titular_admin(admin.ModelAdmin):
-    list_display = ['id', 'nome', 'sobrenome', 'email', 'telefone', 'data_criacao']
+    list_display = ['id', 'nome', 'sobrenome', 'telefone', 'data_criacao']
 
 
 class Conta_admin(admin.ModelAdmin):
     list_display = ['titular', 'numero_conta', 'agencia', 'nome_banco' ,'saldo', 'ativo']
     search_fields = ['titular', 'numero_conta']
 
-admin.site.register(models.Titular, Titular_admin)
-admin.site.register(models.Conta, Conta_admin)
+admin.site.register(Perfil, Titular_admin)
+admin.site.register(Conta, Conta_admin)
+admin.site.register(User)
+# admin.site.register(Perfil)
